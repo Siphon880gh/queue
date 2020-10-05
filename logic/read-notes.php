a19c1b227d676b04ac80cd3603740e9b92cf07fd
Weng Fei Fung <weffung@ucdavis.edu>
2020-10-05 04:35:30 -0700

    Warning: PHP only

--- logic/read-notes.php
+++ logic/read-notes.php
@@ -1,4 +1,7 @@
 <?php
+    /* If you see this message in the textarea, then your server does not support PHP. For eaxmple, Githug Pages does not support PHP. Deploy on a PHP server to save notes and order of checked off items. */
+
+    
     $wrapper = @file_get_contents("../data/notes.json");
     $wrapper = json_decode($wrapper);
     echo $wrapper[0];
